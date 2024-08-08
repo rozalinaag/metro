1d67e5d63fd49552263ea7ea7c9d80640b9f4365
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-08-07 22:33:26 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import video from './assets/video.mp4';
 import deleteVideo from './assets/delete.mp4';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <div>
-        <video autoPlay loop muted width="500" height="500">
-          <source src={video} type="video/mp4" />
-        </video>
+      <div className="root">
+        <div className="phone">
+          <Header />
+
+          <video className="video" autoPlay loop muted width="200" height="200">
+            <source src={video} type="video/mp4" />
+          </video>
+
+          <Footer />
+        </div>
 
         <video controls width="500" height="750">
           <source src={deleteVideo} type="video/mp4" />
