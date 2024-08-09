69d11d483f211ee4369f51a550f5c1f988d7c418
rozalinaag <59352861+rozalinaag@users.noreply.github.com>
2024-08-09 18:39:27 -0500

    dayjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,69 @@
 import video from './assets/video.mp4';
 import deleteVideo from './assets/delete.mp4';
-import './App.css';
+import css from './App.module.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import dayjs from 'dayjs';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+  dayjs.tz.guess();
+  dayjs.tz.setDefault('America/New_York');
+
+  let nowTime = dayjs();
+  console.log(nowTime);
+
+  const [date, setDate] = useState(new Date().getSeconds());
+  const dateRef = useRef(new Date().getSeconds());
+
+  useEffect(() => {
+    const animation = () => {
+      const seconds = dateRef.current;
+      const current_seconds = new Date().getSeconds();
+
+      if (seconds !== current_seconds) {
+        dateRef.current = current_seconds;
+        setDate(current_seconds);
+      }
+
+      window.requestAnimationFrame(animation);
+    };
+
+    window.requestAnimationFrame(animation);
+
+    return () => {
+      cancelAnimationFrame(animation);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="root">
-        <div className="phone">
-          <Header />
+    <div className={css.root}>
+      <div className={css.phone}>
+        <Header />
 
-          <video className="video" autoPlay loop muted width="200" height="200">
-            <source src={video} type="video/mp4" />
-          </video>
-
-          <Footer />
-        </div>
-
-        <video controls width="500" height="750">
-          <source src={deleteVideo} type="video/mp4" />
+        <video
+          className={css.video}
+          autoPlay
+          loop
+          muted
+          width="200"
+          height="200"
+        >
+          <source src={video} type="video/mp4" />
         </video>
+        {nowTime.format('HH:MM:')}
+        {date}
+        <Footer />
       </div>
-    </>
+
+      <video controls width="500" height="750">
+        <source src={deleteVideo} type="video/mp4" />
+      </video>
+    </div>
   );
 }
 
